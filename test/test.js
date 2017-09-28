57f49fa8851bfcf441887af7a450bd819266a64e
Justin Lee <leejwg.3@gmail.com>
2017-09-27 17:03:29 -1000

    started test file for shopping list item

--- test/test.js
+++ test/test.js
@@ -1,1 +1,12 @@
-console.log('test');
+console.log('test file linked');
+
+var chai = require('chai');
+var mocha = require('mocha');
+
+describe('shoppingListItem', function() {
+  it('should be a class', function() {
+    shoppingListItem.expect.to.be.a.function();
+  });
+
+
+}); // end of describe
